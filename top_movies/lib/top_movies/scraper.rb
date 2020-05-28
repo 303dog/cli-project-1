f8f53e0e2e924e9ec08febeef805bc93fb6c3ea6
Paul_Beckwith <303dog@gmail.com>
2020-05-28 16:33:24 -0600

    create class scraper and movie

---
+++ top_movies/lib/top_movies/scraper.rb
@@ -0,0 +1,35 @@
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
+
+class Scraper
+
+    attr_accessor :title, :length, :genre, :summary, :rank, :cast, :directory
+
+  def get_page
+    Nokogiri::HTML(open("https://www.imdb.com/list/ls055592025/"))
+  end
+
+  def scrape_movies_index
+      get_page.css(".lister-item.mode-detail").collect do |node|
+      movie_date = {
+        TITLE:    node.css("h3").text.strip.chomp("\n").strip.gsub("\n","").gsub("  "," "),
+        LENGTH: node.css(".runtime").text,
+        GENRE:    node.css(".genre").text.strip,
+        SUMMARY:  node.css(".lister-item-content p")[1].text.strip,
+        RANK:  node.css(".ipl-rating-star__rating").first.text,
+        CAST:     node.css(".text-muted.text-small")[1].text.split("Stars:")[1].strip.gsub("\n","").gsub("  "," "),
+        DIRECTORY: node.css(".text-muted.text-small")[1].text.split("Stars:")[0].strip.chomp("|").strip.gsub("\n","").gsub("  "," ")
+      }
+      Movie.new(movie_data)
+    end
+  end
+  
+  def make_movies(i)
+    puts ""
+    puts "---------------##{i} of the Top 100----------------"
+    movie = scrape_movies_index[i-=1]
+    puts ""
+    Pry::ColorPrinter.pp(movie)
+  end
+end
